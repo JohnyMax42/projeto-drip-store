25c6651d13ffa57bbe5499a600229a5df051ba05
JohnyMax42 <johnyernesto1997@gmail.com>
2024-07-15 17:15:55 -0300

    commit store

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import './HeaderStyle.css'
+import logo from '../assets/logo.png'
+
+export default function Header(){
+    
+    return(
+         <header className="container">
+            <div id='cabecalho'>
+                <img id="logo" src={logo} alt="logo"/>
+                <input id="barraPesquisa" type="text" placeholder="Pesquisar produto..."/>
+                <input id="botaoLupa" type="button" value=''/>
+                <a id="cadastro" className="links" href="#cadastro">Cadastre-se</a>
+                <input id="botaoEntrar" type="button" value="Entrar"/>
+                <a id="iconeCarrinho" href="#carrinho"></a>
+            </div>
+            <div>
+                <nav id="linksHeader">
+                    <a className="links" href="#home">Home</a>
+                    <a className="links" href="#produtos">Produtos</a>
+                    <a className="links" href="#categorias">Categorias</a>
+                    <a className="links" href="#meusPedidos">Meus Pedidos</a>
+                </nav>
+            </div>
+         </header>
+    )
+}
