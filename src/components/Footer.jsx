5643b7cbb201c6963defa89584147359645247a4
JohnyMax42 <johnyernesto1997@gmail.com>
2024-07-16 15:03:17 -0300

    footer atualizações

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,41 +6,38 @@ export default function Footer(){
     
     return(
         
-         <footer className="container">
+         <footer className='caixaFooter'>
+            <div className='caixaFooterDois'>
+            <div>
                 <img id="logo2" src={logo2} alt="logo2"/>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam blanditiis alias unde neque, minima architecto dolores dolorem?</p>
+                <p id='textoFooter'>Lorem ipsum dolor sit amet, consectetur adipisicing ept. Quam blanditiis apas unde neque, minima architecto dolores dolorem?</p>
                 <img id="redesSociais" src={redesSociais} alt="Redes Sociais"/>
-                <div>
-                    <p><strong>Informação</strong></p>
-                    <ul>
-                        <li>Sobre Drip Store</li>
-                        <li>Segurança</li>
-                        <li>Wishlist</li>
-                        <li>Blog</li>
-                        <li>Trabalhe conosco</li>
-                        <li>Meus Pedidos</li>
-                    </ul>
-                </div>
-                <div>
-                    <p>Categorias</p>
-                    <ul>
-                        <li>Camisetas</li>
-                        <li>Calças</li>
-                        <li>Bonés</li>
-                        <li>Headphones</li>
-                        <li>Tênis</li>
-                    </ul>
-                </div>
-                <div>
-                    <p>Contato</p>
-                    <ul>
-                        <li>Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza - CE, 60150-161</li>
-                        <li>(85) 3051-3411</li>
-                    </ul>
-                </div>
+            </div>
+            <div className='sessoes'>
+                <p className='tituloSessoes'>Informação</p>
+                <p>Sobre Drip Store</p>
+                <p>Segurança</p>
+                <p>Wishpst</p>
+                <p>Blog</p>
+                <p>Trabalhe conosco</p>
+                <p>Meus Pedidos</p>
+            </div>
+            <div className='sessoes'>
+                <p className='tituloSessoes'>Categorias</p>
+                <p>Camisetas</p>
+                <p>Calças</p>
+                <p>Bonés</p>
+                <p>Headphones</p>
+                <p>Tênis</p>
+            </div>
+            <div className='sessoes'>
+                <p className='tituloSessoes'>Contato</p>
+                <p>Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza - CE, 60150-161</p>
+                <p>(85) 3051-3411</p>
+            </div>
+            </div>
                 <hr width="1240px" size="1px"/>
                 <p>@ 2022 Digital College</p>
-
          </footer>
     )
 }
