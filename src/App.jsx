9c98f846dfc9891ff8623a00c57b5241441679d4
JohnyMax42 <johnyernesto1997@gmail.com>
2024-07-16 11:33:53 -0300

    correções

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
+import Footer from './components/Footer'
+
 
 
 function App() {
@@ -10,9 +12,11 @@ function App() {
 
   return (
     <>
+
     <Header></Header>
+    
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+      <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
@@ -31,6 +35,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <Footer></Footer>
+
     </>
   )
 }
