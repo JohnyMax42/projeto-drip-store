ea9a558f183de0ae62191098dd7a32e8076506a8
Misa733 <misaelfernandes2023@gmail.com>
2024-07-17 16:57:12 -0300

    ultima atualização banner

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Banner from './components/Banner'
 import Layout from './pages/Layout'
+import Banner2 from './components/Banner2'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Layout pagina={<Banner />}></Layout>
+      <Layout pagina={<Banner2 />}></Layout>
     </>
   )
 }
