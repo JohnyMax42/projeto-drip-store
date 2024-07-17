a376ba5d677a0e48f5d4546d99430d38bf431976
Levi <levibezerra320@gmail.com>
2024-07-16 21:01:50 -0300

    fix

--- src/components/Banner2.jsx
+++ src/components/Banner2.jsx
@@ -9,7 +9,7 @@ export default function Banner2() {
                 <div className="Banner-container">
                     <img className="imagem" src={Shoes2} alt="shoes" />
                     <div className="texto">
-                        <p>Oferta especial</p>
+                        <p className="p">Oferta especial</p>
                         <h1>Air Jordan edição de colecionador</h1>
                         <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</span>
                         {/* <input id="botaoEntrar" type="button" value="Ver Ofertas" /> */}
