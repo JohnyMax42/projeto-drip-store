5fe967c401ed4d550468f952e42997fe32cb4fd7
JohnyMax42 <johnyernesto1997@gmail.com>
2024-07-17 17:05:50 -0300

    pagina 2 commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,17 @@ import './App.css'
 import Banner from './components/Banner'
 import Banner2 from './components/Banner2'
 import Layout from './pages/Layout'
-import Banner2 from './components/Banner2'
+import ProductListingPage from './pages/ProductListingPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<<<<<<< HEAD
       <Layout pagina={<Banner />}></Layout>
       <Layout pagina={<Banner2 />}></Layout>
-=======
       <Layout pagina={<div><Banner /><Banner2 /></div>} />
->>>>>>> 836722ef438d6ad8aa9ee849301431f93acbcd9a
+      <Layout pagina={<ProductListingPage />}></Layout>
     </>
   )
 }
