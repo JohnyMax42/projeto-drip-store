84a4b8d0ce19dc63108b26cb4589bf8ec5e90c89
Levi <levibezerra320@gmail.com>
2024-07-17 14:49:39 -0300

    stylized cores

---
+++ src/components/Cores.jsx
@@ -0,0 +1,15 @@
+const cores = {
+    primary: '#C92071',
+    secondary: '#B5B6F2',
+    tertiary: '#991956',
+    error: '#EE4266',
+    success: '#52CA76',
+    warning: '#F6AA1C',
+    darkGray: '#1F1F1F',
+    darkGray2: '#474747',
+    darkGray3: '#666666',
+    lightGray: '#8F8F8F',
+    lightGray2: '#CCCCCC',
+    lightGray3: '#F5F5F5',
+    white: '#FFFFFF'
+};
