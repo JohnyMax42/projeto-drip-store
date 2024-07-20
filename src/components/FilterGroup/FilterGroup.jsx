f523ad1ba0241b5f7780d705070f696a47d73285
JohnyMax42 <johnyernesto1997@gmail.com>
2024-07-20 02:05:35 -0300

    correção nos filtros

--- src/components/FilterGroup/FilterGroup.jsx
+++ src/components/FilterGroup/FilterGroup.jsx
@@ -2,50 +2,43 @@ import React from "react";
 import styled from "styled-components";
 import Cores from "../Cores";
 
-//estilo do input
+//estilo do titulo
 const Titulo = styled.div`
     color: ${Cores.darkGray2};
     font-size: bold 14px;
+    font-weight: 700;
+    margin-top: 20px;
+`;
+
+const DivStyle = styled.div`
+    color: ${Cores.darkGray2};
+    font-size: bold 14px;
+    font-weight: 500;
+    width: auto;
+    margin-top: 10px;
+    display: flex;
+    gap: 10px;
+`;
+
+//estilo dos inputs
+const InputStyle = styled.input`
+    width: 22px;
+    height: 22px;
+    //visibility: hidden;
+    //background-color: white;
+    &:checked{
+    background-color: ${Cores.primary};
+    color: white;
+    }
 `;
 
 
-//escolha do input type checkbox ou radio
-function EscolhaInput({escolha}){
-
-    let Resultado
-
-    if(escolha=='checkbox'){
-        Resultado = ({})=>{
-            return(
-            <>
-            <input type="checkbox" id="cb1" />
-            <label htmlFor="cb1">label teste checkbox</label>
-            </>
-            )
-        }
-    } else if(escolha=='radio'){
-        Resultado = ({})=>{
-            return(
-            <>
-            <input type="radio" name="r1" id="r1" />
-            <label htmlFor="r1">label teste radio</label>
-            </>
-            )
-        }
-    }
-    return(
-        <>
-        <p>{<Resultado/>}</p>
-       </> 
-    )
-}
-
 //opções de categorias dos filtros
-function EscolhaOption({escolha}){
+function EscolhaOption({escolher, inputType}){
 
     let Opcoes = []
-
-    if(escolha==('Marka'||'marka')){
+    
+    if(escolher==('marca')){
         Opcoes = [
             {texto: 'Adiddas'},
             {texto: 'Calenciada'},
@@ -53,41 +46,42 @@ function EscolhaOption({escolha}){
             {texto: 'Nike'},
             {texto: 'Puma'}
         ]
-    } else if(escolha==('Categoria'||'categoria')){
+    } else if(escolher==('categoria')){
         Opcoes = [
             {texto: 'Esporte e lazer'},
             {texto: 'Casual'},
             {texto: 'Utilitário'},
             {texto: 'Corrida'}
         ]
-    } else if(escolha==('Gênero'||'gênero')){
+    } else if(escolher==('gênero')){
         Opcoes = [
             {texto: 'Masculino'},
             {texto: 'Feminino'},
             {texto: 'Unisex'}
         ]
-    } else if(escolha==('Estado'||'estado')){
+    } else if(escolher==('estado')){
         Opcoes = [
             {texto: 'Novo'},
             {texto: 'Usado'}
         ]
     }
-
-    let ObjetosFiltros = Opcoes.map((item)=>item.texto)
-
     return(
         <>
-        <p>{<ObjetosFiltros/>}</p>
+        {Opcoes.map((opcao, index) => (
+        <DivStyle key={index}>
+          <InputStyle type={inputType} name={`Type${inputType}`} id={`${escolher}-${index}`} value={opcao.texto}/>
+          <label htmlFor={`${escolher}-${index}`}>{opcao.texto}</label>
+        </DivStyle>
+      ))}
        </> 
     )
-}
+};
 
 export default function FilterGroup({title, inputType, option}){
     return (
     <>
         <Titulo>{title}</Titulo>
-        <EscolhaInput escolha={inputType}/>
-        {/* <EscolhaOption escolha={option}/> */}
+        <EscolhaOption escolher={option} inputType={inputType}/>
     </>
-    )
+    );
 }
