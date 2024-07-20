6faaa06dec4b24eb9dfd2e6dd60d20c7a8e1fdab
Misael <misaelfernandes2023@gmail.com>
2024-07-20 02:20:05 -0300

    correções

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,18 @@
 import './FooterStyle.css'
 import logo2 from '../../assets/logo2.png'
-// import redesSociais from '../src/assets/redesSociais.png'
+import styled from 'styled-components'
+import Cores from "../Cores";
+import redesSociais from '../../assets/redesSociais.png'
+
+const Link = styled.a`
+    color: white;
+
+    &:hover{
+        color: ${Cores.primary};
+    }
+    
+    
+`
 
 export default function Footer() {
 
@@ -11,13 +23,13 @@ export default function Footer() {
                 <div>
                     <img id="logo2" src={logo2} alt="logo2" />
                     <p id='textoFooter'>Lorem ipsum dolor sit amet, consectetur adipisicing ept. Quam blanditiis apas unde neque, minima architecto dolores dolorem?</p>
-                    {/* <img id="redesSociais" src={redesSociais} alt="Redes Sociais" /> */}
+                    <img id="redesSociais" src={redesSociais} alt="Redes Sociais" />
                 </div>
                 <div className='sessoes'>
                     <p className='tituloSessoes'>Informação</p>
                     <p>Sobre Drip Store</p>
                     <p>Segurança</p>
-                    <p>Wishpst</p>
+                    <p>Wishlist</p>
                     <p>Blog</p>
                     <p>Trabalhe conosco</p>
                     <p>Meus Pedidos</p>
@@ -32,8 +44,8 @@ export default function Footer() {
                 </div>
                 <div className='sessoes'>
                     <p className='tituloSessoes'>Contato</p>
-                    <p>Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza - CE, 60150-161</p>
-                    <p>(85) 3051-3411</p>
+                    <Link style={{ paddingBottom: 10 }} href="https://www.google.com.br/maps/place/Manhattan+Square+Garden/@-3.7327059,-38.5130634,17z/data=!3m1!4b1!4m6!3m5!1s0x7c7485ec14d7791:0xe0279af6a0507e9!8m2!3d-3.7327113!4d-38.5104885!16s%2Fg%2F11bw2_h04b?hl=pt-BR&entry=ttu" target="_blank">Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza - CE, 60150-161</Link>
+                    <p style={{ paddingTop: 10 }}>(85) 3051-3411</p>
                 </div>
             </div>
             <hr id='linhaFooter' width="1240px" size="1px" />
