41729dcf078d190b6bb10b3f80f72565877f73c1
JohnyMax42 <johnyernesto1997@gmail.com>
2024-07-23 11:44:48 -0300

    carrossel pagina 3

---
+++ src/components/Carrossel2.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Carousel from 'react-bootstrap/Carousel'
+import Banner from "./Banner";
+// Imagens 
+import image1 from '../assets/product-view-slide.png';
+import image2 from '../assets/product-view-slide.png';
+import image3 from '../assets/product-view-slide.png';
+import image4 from '../assets/product-view-slide.png';
+import image5 from '../assets/product-view-slide.png';
+
+function Carrossel2() {
+
+  const images = [
+    { src: image1, alt: "First Slide" },
+    { src: image2, alt: "Second Slide" },
+    { src: image3, alt: "Third Slide" },
+    { src: image4, alt: "Fourth Slide" },
+    { src: image5, alt: "Fifth Slide" }
+  ];
+
+  return (
+    <div>
+      <Carousel variant="dark" fade controls keyboard={true} interval={null} pause="hover" slide={true} style={{ borderRadius: 4, width: 700, height: 570, display:'flex', alignItems:'center', backgroundColor: '#E2E3FF'}}>
+
+        {images.map((image, index) => (
+          <Carousel.Item key={index}>
+            <img className="" src={image.src} alt={image.alt}/>
+            <Carousel.Caption></Carousel.Caption>      
+          </Carousel.Item>
+        ))}
+
+      </Carousel>
+    </div >
+  )
+}
+
+export default Carrossel2
--- src/pages/ProductViewPage.jsx
+++ src/pages/ProductViewPage.jsx
@@ -2,6 +2,7 @@ import './ProductView.css'
 import ProductListing3 from "../components/ProductListing/ProductListing3"
 import styled from 'styled-components'
 import React, { useState } from 'react';
+import Carrossel2 from "../components/Carrossel2"
 
 export default function ProductViewPage() {
 
@@ -22,20 +23,24 @@ export default function ProductViewPage() {
                 <div className='extrasPagina'>
                     <p><strong>Home</strong> / Produtos / Tênis / Nike / Tênis Nike Revolution 6 Next Nature Masculino</p>
                 </div>
+                
+                <div className='lateralidade'>
 
-                <div id="InfoSapato">
-                    <h2>Tênis Nike Revolution 6 Next Nature Masculino</h2>
-                    <p>Casual | Nike | REF:38416711</p>
-                    <div className='lateralidade'>
-                        <img src="src/assets/Stars.png" alt="estrelas" />
-                        <p className='bg-warning paragrafo-estrela'>4.7{<img src='src/assets/star1.png' alt='estrela' />}</p>
-                        <p>(90 avaliações)</p>
-                    </div>
-                    <h2>R$219,00</h2>
-                    <p>descrição do produto</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-                    <p>Tamanho</p>
-                    <div className='lateralidade'>
+                    <Carrossel2></Carrossel2>
+
+                    <div id="InfoSapato">
+                        <h2>Tênis Nike Revolution 6 Next Nature Masculino</h2>
+                        <p>Casual | Nike | REF:38416711</p>
+                        <div className='lateralidade'>
+                            <img src="src/assets/Stars.png" alt="estrelas" />
+                            <p className='bg-warning paragrafo-estrela'>4.7{<img src='src/assets/star1.png' alt='estrela' />}</p>
+                            <p>(90 avaliações)</p>
+                        </div>
+                        <h2>R$219,00</h2>
+                        <p>descrição do produto</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                        <p>Tamanho</p>
+                        <div className='lateralidade'>
                         <button
                             className={selectedSize === '39' ? 'selected' : ''}
                             onClick={() => handleSizeChange('39')}
@@ -65,6 +70,7 @@ export default function ProductViewPage() {
                         <input className = "escolhacor" type="radio" name="corSapato" id="corRoxo" />
                     </div>
                     <button className='bg-warning text-light'>COMPRAR</button>
+                </div>
                 </div>
 
                 <div className='extrasPagina'>
