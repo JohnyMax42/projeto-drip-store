aab0b8f1c14de063618c5097e412d20b26e7b7bd
Levi <levibezerra320@gmail.com>
2024-07-23 13:59:34 -0300

    Login feito de usuários

--- src/components/Login/LoginCard.jsx
+++ src/components/Login/LoginCard.jsx
@@ -4,6 +4,47 @@ import { styled } from "styled-components"
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
+const Wrapper = styled.div`
+        display: grid;
+        flex-flow: column wrap;
+        justify-content: center;
+        width: 583px;
+        height: auto;
+        /* border: 1px solid; */
+        text-align: left;
+        padding: 30px;
+        background-color: #FFFFFF;
+        border-radius: 7px;
+    `
+
+const WrapperTitulo = styled.div`
+        font-weight: 700;
+    `
+
+const InputLogin = styled.input`
+        color: black;
+        width: 100%;
+        height: 60px;
+        border: 0px;
+        border-radius: 7px;
+        padding-inline-start: 25px;
+        background-color: rgba(71, 71, 71, 0.04);
+    `
+
+const WrapperDados = styled.div`
+        font-weight: 700;
+    `
+
+const BotaoAcessar = styled.button`
+        width: 100%;
+        height: 48px;
+        margin-top: 20px;
+        color: rgba(245, 245, 245, 1);
+        background-color: var(--cor-primaria);
+        border: 0px;
+        border-radius: 8px;
+    `
+
 export default function LoginCard() {
 
     const firebaseConfig = {
@@ -19,63 +60,46 @@ export default function LoginCard() {
     const app = initializeApp(firebaseConfig);
 
     const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
 
     const handleEmailChange = (event) => {
         setEmail(event.target.value);
     }
-    const handleGmailLogin = async () => {
+
+    const handleSenhaChange = (event) => {
+        setSenha(event.target.value);
+    }
+
+    // const handleGmailLogin = async () => {
+    //     try {
+    //         const auth = getAuth();
+    //         const email = ''; // Pode ser um input do usuário
+    //         const senha = ''; // Pode ser um input do usuário
+
+    //         await signInWithEmailAndPassword(auth, email, senha);
+    //         // Autenticação bem-sucedida! Redirecione o usuário para a página principal.
+    //     } catch (error) {
+    //         console.error('Erro ao fazer login:', error);
+    //     }
+    // };
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
         try {
             const auth = getAuth();
-            const email = setEmail; // Pode ser um input do usuário
-            const senha = 'sua-senha'; // Pode ser um input do usuário
-
             await signInWithEmailAndPassword(auth, email, senha);
-            // Autenticação bem-sucedida! Redirecione o usuário para a página principal.
+            console.log('Usuário logado com sucesso:', auth.currentUser.email);
+            // Redirecione ou execute outras operações após o login bem-sucedido
         } catch (error) {
             console.error('Erro ao fazer login:', error);
+            // Trate especificamente o erro de email inválido
+            if (error.code === 'auth/invalid-email') {
+                alert('Endereço de email inválido. Verifique e tente novamente.');
+            } else {
+                alert('Ocorreu um erro ao fazer login. Por favor, tente novamente mais tarde.');
+            }
         }
     };
-
-    const Wrapper = styled.div`
-        display: grid;
-        flex-flow: column wrap;
-        justify-content: center;
-        width: 583px;
-        height: auto;
-        /* border: 1px solid; */
-        text-align: left;
-        padding: 30px;
-        background-color: #FFFFFF;
-        border-radius: 7px;
-    `
-
-    const WrapperTitulo = styled.div`
-        font-weight: 700;
-    `
-
-    const InputLogin = styled.input`
-        color: black;
-        width: 100%;
-        height: 60px;
-        border: 0px;
-        border-radius: 7px;
-        padding-inline-start: 25px;
-        background-color: rgba(71, 71, 71, 0.04);
-    `
-
-    const WrapperDados = styled.div`
-        font-weight: 700;
-    `
-
-    const BotaoAcessar = styled.button`
-        width: 100%;
-        height: 48px;
-        margin-top: 20px;
-        color: rgba(245, 245, 245, 1);
-        background-color: var(--cor-primaria);
-        border: 0px;
-        border-radius: 8px;
-    `
 
     return (
         <Wrapper>
@@ -85,17 +109,30 @@ export default function LoginCard() {
             </WrapperTitulo>
 
 
-            <form action="post">
+            <form onSubmit={handleLogin}>
                 <WrapperDados>
-                    <label htmlFor="" style={{ paddingBottom: '5px' }}>Login *</label>
-                    <InputLogin placeholder="Insira seu login ou email" type="email" onChange={handleEmailChange} value={email} />
-                    <label htmlFor="" style={{ paddingTop: '20px', paddingBottom: '5px' }}>Senha *</label>
-                    <InputLogin placeholder="Insira sua senha" type="password" />
+                    <label htmlFor="emailInput" style={{ paddingBottom: '5px' }}>Email *</label>
+                    <InputLogin
+                        id="emailInput"
+                        placeholder="Insira seu email"
+                        type="text"
+                        value={email}
+                        onChange={handleEmailChange}
+                    />
+                    <label htmlFor="senhaInput" style={{ paddingTop: '20px', paddingBottom: '5px' }}>Senha *</label>
+                    <InputLogin
+                        id="senhaInput"
+                        placeholder="Insira sua senha"
+                        type="password"
+                        value={senha}
+                        onChange={handleSenhaChange}
+                    />
                     <a href="">Esqueci minha senha</a>
+                    <BotaoAcessar>Acessar Conta</BotaoAcessar>
                 </WrapperDados>
-                <BotaoAcessar>Acessar Conta</BotaoAcessar>
+
                 <p>Ou faça login com:
-                    <img src={Gmail} alt="gmail" onClick={handleGmailLogin} />
+                    <img src={Gmail} alt="gmail" />
                 </p>
             </form>
 
