24f047c5f55815b73d8048eafb9af24ac03c6d7d
JohnyMax42 <johnyernesto1997@gmail.com>
2024-07-25 22:19:33 -0300

    responsividade footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -48,7 +48,9 @@ export default function Footer() {
     return (
         <>
             <div className='containerPrincipal'>
-                <div>
+
+                <div id='caixaSuperior'>
+                    <div>
                     <img id="logoFooter" src={logo2} alt="logo2" />
                     <p id='textoFooter'>Lorem ipsum dolor sit amet, consectetur adipisicing ept. Quam blanditiis apas unde neque, minima architecto dolores dolorem?</p>
                     <div className="redesSociais">
@@ -56,16 +58,18 @@ export default function Footer() {
                         <a href={Instalink} target="_blank" title='Instagram' style={{ paddingRight: 35, paddingLeft: 35 }}><img src={Instagram} alt="Instagram" /></a>
                         <a href={xlink} target="_blank" title='X'><img src={X} alt="X" /></a>
                     </div>
-                </div>
+                    </div>
 
-                <InformationComponent title={'Informação'} informations={['Sobre Drip Store', 'Segurança', 'Wishlist', 'Blog', 'Trabalhe Conosco', 'Meus Pedidos']} />
-                <InformationComponent title={'Categorias'} informations={['Camisetas', 'Calças', 'Bonés', 'Headphones', 'Tênis']} />
-                <InformationComponent title={'Contato'} informations={[<a href="https://maps.app.goo.gl/FWFsV2bRwoEw7SpD9" target="_blank"><Infos>Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza - CE, 60150-161</Infos></a>, '(85) 3051-3411']} />
+                    <InformationComponent title={'Informação'} informations={['Sobre Drip Store', 'Segurança', 'Wishlist', 'Blog', 'Trabalhe Conosco', 'Meus Pedidos']} />
+                    <InformationComponent title={'Categorias'} informations={['Camisetas', 'Calças', 'Bonés', 'Headphones', 'Tênis']} />
+                    <InformationComponent title={'Contato'} informations={[<a href="https://maps.app.goo.gl/FWFsV2bRwoEw7SpD9" target="_blank"><Infos>Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza - CE, 60150-161</Infos></a>, '(85) 3051-3411']} />
+                </div>
 
                 <div id='caixaInferior'>
-                    <hr id='linhaFooter' width="1240px" />
+                    <hr id='linhaFooter'/>
                     <p>@ 2022 Digital College</p>
                 </div>
+
             </div>
         </>
     )
