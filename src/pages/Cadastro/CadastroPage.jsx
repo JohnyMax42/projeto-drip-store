e9d670c062a5f67d3132b828316f2737c8f5367f
Levi <levibezerra320@gmail.com>
2024-07-26 13:10:28 -0300

    Ajustes na tela de cadastro e também colocado os itens em linha

--- src/pages/Cadastro/CadastroPage.jsx
+++ src/pages/Cadastro/CadastroPage.jsx
@@ -16,33 +16,29 @@ export default function CadastroPage() {
   box-sizing: border-box; /* Inclui padding no cálculo do tamanho total */`
 
     const BotaoAcessar = styled.button`
-width: 750px;
-height: 48px;
-margin-top: 20px;
-color: rgba(245, 245, 245, 1);
-background-color: var(--cor-primaria);
-border: 0px;
-border-radius: 8px;`
-
-const InputBox = styled.input`
-width: 22px;
-height: 22px;
-border-radius: 4px;
-background-color: '#FF5733','#4CAF50';
-transition: background-color 0.3s ease, border-color 0.3s ease;
+        width: 750px;
+        height: 48px;
+        margin: 20px 0px 20px 0px;
+        color: rgba(245, 245, 245, 1);
+        background-color: var(--cor-primaria);
+        border: 0px;
+        border-radius: 8px;
 `
 
-
-
-
-
+    const InputBox = styled.input`
+        width: 22px;
+        height: 22px;
+        border-radius: 4px;
+        background-color: '#FF5733','#4CAF50';
+        transition: background-color 0.3s ease, border-color 0.3s ease;
+`
     return (
         <Container>
             <p className="CadastroTitulo">Criar Conta</p>
             <CadastroCard></CadastroCard>
-           <div className="container">
-            <InputBox></InputBox> 
-            <p>Quero receber por email ofertas e novidades das lojas da Digital Store. A frequência de envios pode variar de acordo com a interação do cliente</p>
+            <div className="containerCadastro">
+                <InputBox type="checkbox"></InputBox>
+                <p>Quero receber por email ofertas e novidades das lojas da Digital Store. A frequência de envios pode variar de acordo com a interação do cliente</p>
             </div>
             <BotaoAcessar>Criar Conta</BotaoAcessar>
         </Container>
